fbc3c094a3755e3c16190ca95b25ec0555e7fc4d
박동진 <peter9378@naver.com>
2020-08-17 14:57:08 +0900

    fix conflict

---
+++ src/main/java/com/ddd/justeatit/JusteatitApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.justeatit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusteatitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JusteatitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddd/justeatit/JusteatitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddd.justeatit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JusteatitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
