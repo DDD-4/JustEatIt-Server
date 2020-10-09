a867ea064f1d56aa9112bb26792c58ccca9f7ea5
박동진 <peter9378@naver.com>
2020-10-10 00:09:57 +0900

    refactor UsersService to UserService

--- src/main/java/com/ddd/justeatit/service/UserService.java
+++ src/main/java/com/ddd/justeatit/service/UserService.java
@@ -3,6 +3,7 @@ package com.ddd.justeatit.service;
 import com.ddd.justeatit.dao.UserDao;
 import com.ddd.justeatit.dto.UserDto;
 import lombok.AllArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,7 @@ public class UsersService {
     @Autowired
     private UserDao userDao;
 
-    public ResponseEntity<UserDto> createUser(String userId, String userName, String userEmail) {
-        UserDto userDto = userDao.
+    public ResponseEntity<UserDto> createUser(JSONObject user) {
+        UserDto userDto = new UserDto(user.getString("userId"), user.get)
     }
 }
