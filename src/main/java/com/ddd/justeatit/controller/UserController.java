d6cf585ca18a70df2e21aa52878b1f4e52f8c2c9
박동진 <peter9378@naver.com>
2020-10-10 02:31:40 +0900

    add user service and controller

--- src/main/java/com/ddd/justeatit/controller/UserController.java
+++ src/main/java/com/ddd/justeatit/controller/UserController.java
@@ -1,17 +1,47 @@
 package com.ddd.justeatit.controller;
 
-import com.ddd.justeatit.service.UsersService;
+import com.ddd.justeatit.dto.UserDto;
+import com.ddd.justeatit.dto.UserPreferInfoDto;
+import com.ddd.justeatit.dto.UserVisitInfoDto;
+import com.ddd.justeatit.service.UserService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
-    private UsersService usersService;
+    private UserService userService;
 
+//    @PostMapping
+//    @ResponseBody
+//    public int createUser(@RequestBody JSONObject user, HttpServletRequest req) throws Exception {
+//        userService.createUser(user);
+//    }
 
+    @GetMapping(value = "/{userId}")
+    @ResponseBody
+    public ResponseEntity<UserDto> readUser(@PathVariable("userId") String userId, HttpServletRequest req) throws Exception {
+        return userService.readUserByUserId(userId);
+    }
 
+    @GetMapping(value = "/{userId}/userFriend")
+    public ResponseEntity<List<String>> readUserFriend(@PathVariable("userId") String userId, HttpServletRequest req) throws Exception {
+        return userService.readUserFriendByUserId(userId);
+    }
 
+    @GetMapping(value = "/{userId}/userPreferInfo")
+    public ResponseEntity<UserPreferInfoDto> readUserPreferInfo(@PathVariable("userId") String userId, HttpServletRequest req) throws Exception {
+        return userService.readUserPreferInfoByUserId(userId);
+    }
 
+    @GetMapping(value = "/{userId}/userVisitInfo")
+    public ResponseEntity<UserVisitInfoDto> readUserVisitInfo(@PathVariable("userId") String userId, HttpServletRequest req) throws Exception {
+        return userService.readUserVisitInfoByUserId(userId);
+    }
 }
--- src/main/java/com/ddd/justeatit/service/UserService.java
+++ src/main/java/com/ddd/justeatit/service/UserService.java
@@ -2,19 +2,45 @@ package com.ddd.justeatit.service;
 
 import com.ddd.justeatit.dao.UserDao;
 import com.ddd.justeatit.dto.UserDto;
+import com.ddd.justeatit.dto.UserPreferInfoDto;
+import com.ddd.justeatit.dto.UserVisitInfoDto;
 import lombok.AllArgsConstructor;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @AllArgsConstructor
 @Service
-public class UsersService {
+public class UserService {
     @Autowired
     private UserDao userDao;
 
-    public ResponseEntity<UserDto> createUser(JSONObject user) {
-        UserDto userDto = new UserDto(user.getString("userId"), user.get)
+
+    public ResponseEntity<UserDto> readUserByUserId(String userId) {
+        UserDto userDto = userDao.readUserByUserId(userId);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<String>> readUserFriendByUserId(String userId) {
+        UserDto userDto = userDao.readUserByUserId(userId);
+        List<String> userFriends = userDto.getUserFriend();
+        return new ResponseEntity<>(userFriends, HttpStatus.OK);
+    }
+
+    public ResponseEntity<UserPreferInfoDto> readUserPreferInfoByUserId(String userId) {
+        UserDto userDto = userDao.readUserByUserId(userId);
+        UserPreferInfoDto userPreferInfo = userDto.getUserPreferInfo();
+        return new ResponseEntity<>(userPreferInfo, HttpStatus.OK);
+    }
+
+    public ResponseEntity<UserVisitInfoDto> readUserVisitInfoByUserId(String userId) {
+        UserDto userDto = userDao.readUserByUserId(userId);
+        UserVisitInfoDto userVisitInfo = userDto.getUserVisitInfo();
+        return new ResponseEntity<>(userVisitInfo, HttpStatus.OK);
     }
 }
